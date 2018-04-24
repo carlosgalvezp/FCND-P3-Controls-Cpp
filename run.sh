90f35d4b26f53b0e9c85db1cefea3df30c3ef877
Carlos Galvez <carlosgalvezp@gmail.com>
2018-04-24 11:13:21 +0200

    Create separate build script

--- run.sh
+++ run.sh
@@ -4,12 +4,7 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 BUILD_DIR="${REPO_ROOT}/build"
 
 # Build code
-mkdir -p "${BUILD_DIR}"
-
-docker run --rm -it --volume="${REPO_ROOT}":"${REPO_ROOT}" --workdir="${BUILD_DIR}" \
-           --user=$(id -u):$(id -g) carlosgalvezp/fcnd_p3_cpp cmake ..
-docker run --rm -it --volume="${REPO_ROOT}":"${REPO_ROOT}" --workdir="${BUILD_DIR}" \
-           --user=$(id -u):$(id -g) carlosgalvezp/fcnd_p3_cpp make -j8
+"${REPO_ROOT}/build.sh"
 
 # Run simulator
 # https://github.com/NVIDIA/nvidia-docker/issues/136#issuecomment-232755805
